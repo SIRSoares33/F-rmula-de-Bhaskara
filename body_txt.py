b63142e1c401d950b4c6a36d6da26f30bf14f342
Gustavo Soares <sirgustavo4@gmail.com>
2023-02-12 15:14:45 -0300

    Corpo do arquivo txt (passo a passo)

---
+++ body_txt.py
@@ -0,0 +1,28 @@
+def body_txt(a, b, c, delta, raiz, division, x1, x2):
+    archive = open("Passo a passo.txt", "a", encoding="UTF-8")
+    archive.write("                DELTA:")
+    archive.write("\n")
+    archive.write("\n")
+    archive.write(f"Delta = {b}² - (4 x {a} x {c}) ")
+    archive.write("\n")
+    archive.write(f"Delta = {delta}")
+    archive.write("\n")
+    archive.write("\n")
+    archive.write("                CALCULAR X:")
+    archive.write("\n")
+    archive.write("\n")
+    archive.write(f"X1 = -({b})² + raiz({delta}) dividido({division})")
+    archive.write("\n")
+    archive.write(f"X1 = {b ** 2} + {raiz} dividido({division})")
+    archive.write("\n")
+    archive.write(f"X1 = {x1}")
+    archive.write("\n")
+    archive.write("\n")
+    archive.write(f"X2 = -({b})² - raiz({delta}) dividido({division})")
+    archive.write("\n")
+    archive.write(f"X2 = {b ** 2} - {raiz}")
+    archive.write("\n")
+    archive.write(f"X2 = {x2}")
+
+
+    
